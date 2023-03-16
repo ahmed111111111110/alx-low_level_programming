e5dff9b7c88918ed0ab9824ac80db181cba548f6
ahmed111111111110 <ahmedhuseeny1.gmail.com>
2023-03-16 05:26:39 -0700

    hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+
 #include <unistd.h>
 /**
  * main - Prints out the last part of a quote in the standard error.
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
-	return (1);
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+    return (1);
 }
