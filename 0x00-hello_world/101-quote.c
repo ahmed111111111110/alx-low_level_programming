2e91cc25926e8621258a8953c1ae8e4ec51e4ecd
ahmed111111111110 <ahmedhuseeny1.gmail.com>
2023-03-16 05:16:05 -0700

    hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,68 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Prints out the last part of a quote in the standard error.
  * Return: 1 if success.
  */
 int main(void)
 {
-        putchar('a');
-        putchar('n');
-        putchar('d');
-        putchar(' ');
-        putchar('t');
-        putchar('h');
-        putchar('a');
-        putchar('t');
-        putchar(' ');
-        putchar('p');
-        putchar('i');
-        putchar('e');
-        putchar('c');
-        putchar('e');
-        putchar(' ');
-        putchar('o');
-        putchar('f');
-        putchar(' ');
-        putchar('a');
-        putchar('r');
-        putchar('t');
-        putchar(' ');
-        putchar('i');
-        putchar('s');
-        putchar(' ');
-        putchar('u');
-        putchar('s');
-        putchar('e');
-        putchar('f');
-        putchar('u');
-        putchar('l');
-        putchar('"');
-        putchar(' ');
-        putchar('-');
-        putchar(' ');
-        putchar('D');
-        putchar('o');
-        putchar('r');
-        putchar('a');
-        putchar(' ');
-        putchar('K');
-        putchar('o');
-        putchar('r');
-        putchar('p');
-        putchar('a');
-        putchar('r');
-        putchar(',');
-        putchar(' ');
-        putchar('2');
-        putchar('0');
-        putchar('1');
-        putchar('5');
-        putchar('-');
-        putchar('1');
-        putchar('0');
-        putchar('-');
-        putchar('1');
-        putchar('9');
-        putchar('\n');
-    return (1);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }
