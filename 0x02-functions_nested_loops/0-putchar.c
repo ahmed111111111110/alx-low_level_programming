162b90b1b7c4bac76a17a1227a190df10cef8939
ahmed111111111110 <ahmedhuseeny1@gmail.com>
2023-03-21 18:51:15 +0200

    My commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - prints the alphabet in reverse.
-(*
- * Return: 0 on success
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
 int main(void)
 {
-printf("%s", "_putchar\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
+
