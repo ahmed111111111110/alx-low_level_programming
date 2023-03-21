ad106bf5de2f2ea56aca6cea4edc98a60c4bc048
ahmed111111111110 <ahmedhuseeny1@gmail.com>
2023-03-21 19:04:02 +0200

    My commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: Ahmed-Hussieny 0000000000000
+ * Return: 1
+ */
+int _isalpha(int c)
+{
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
