a4b4118f5864d71b2a17109e5052cf5449e617d5
ahmed111111111110 <ahmedhuseeny1@gmail.com>
2023-03-28 01:10:25 +0200

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+int c = strlen(s);
+return (c);
+}
