9413deae201a106ba79e35bd51b18960ea26e28d
ahmed111111111110 <ahmedhuseeny1@gmail.com>
2023-03-28 01:33:01 +0200

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
 /**
   * rev_string - Reverses a string
   * @s: The string to be modified
@@ -6,26 +9,22 @@
   */
 void rev_string(char *s)
 {
-	int i, c, k;
-	char *a, aux;
-
-	a = s;
-
-	while (s[c] != '\0')
-	{
-		c++;
-	}
-
-	for (k = 1; k < c; k++)
-	{
-		a++;
-	}
-
-	for (i = 0; i < (c / 2); i++)
-	{
-		aux = s[i];
-		s[i] = *a;
-		*a = aux;
-		a--;
-	}
+int i, c, k;
+char *a, aux;
+a = s;
+while (s[c] != '\0')
+{
+c++;
+}
+for (k = 1; k < c; k++)
+{
+a++;
+}
+for (i = 0; i < (c / 2); i++)
+{
+aux = s[i];
+s[i] = *a;
+*a = aux;
+a--;
+}
 }
