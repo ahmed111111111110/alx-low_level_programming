7b38bc0c80a295749b3a37891e4b3b7e0f8f7504
ahmed111111111110 <ahmedhuseeny1@gmail.com>
2023-04-05 17:28:02 +0200

    recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,19 +1,17 @@
 #include "main.h"
+
 /**
-*_pow_recursion - Prints a power of any number
-*@x : the base number
-*@y : the pow number
-*
-* Return: pow number
-*/
+ * _pow_recursion - number x of power  y
+ * @x: base Number
+ * @y: Power
+ *
+ * Return: number x of power  y.
+ */
 int _pow_recursion(int x, int y)
 {
+if (y < 0)
+return (-1);
 if (y == 0)
-{
 return (1);
-}
-else
-{
 return (x * _pow_recursion(x, y - 1));
-}
 }
