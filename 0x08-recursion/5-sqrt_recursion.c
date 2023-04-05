482622b0a029d3edb9c19070fb7f7d70e12209bf
ahmed111111111110 <ahmedhuseeny1@gmail.com>
2023-04-05 17:24:24 +0200

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,26 +1,17 @@
 #include "main.h"
+
 /**
-* sqrt2 - Makes possible to evaluate from 1 to n
-* @a: same number as n
-* @b: number that iterates from 1 to n
-*
-* Return: sqrt number
-*/
-int sqrt2(int a, int b)
+ * _pow_recursion - number x of power  y
+ * @x: base Number
+ * @y: Power
+ *
+ * Return: number x of power  y.
+ */
+int _pow_recursion(int x, int y)
 {
-if (b * b == a)
-return (b);
-else if (b * b > a)
+if (y < 0)
 return (-1);
-return (sqrt2(a, b + 1));
-}
-/**
-* _sqrt_recursion - returns the natural square root of n
-* @n: Number Integer
-*
-* Return: On success 1.
-*/
-int _sqrt_recursion(int n)
-{
-return (sqrt2(n, 1));
+if (y == 0)
+return (1);
+return (x * _pow_recursion(x, y - 1));
 }
