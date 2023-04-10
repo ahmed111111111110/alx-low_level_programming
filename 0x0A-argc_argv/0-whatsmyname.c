6e201ec5141154166376416b9e13b186c9b0e5ce
ahmed111111111110 <ahmedhuseeny1@gmail.com>
2023-04-11 00:01:14 +0200

    arg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+* main - Print name of program
+* @argc: Number if lines arguments
+* @argv: Argument Vector
+* Return: 0
+*/
 int main (int argc, char *argv[])
 {
-printf ("%s\".\n",argv[0]);
+printf ("%s\n",argv[0]);
 return 0;
 }
