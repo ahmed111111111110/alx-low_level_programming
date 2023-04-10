8e46d1851115356124ebb5c7bfe72ea630015da3
ahmed111111111110 <ahmedhuseeny1@gmail.com>
2023-04-11 00:22:32 +0200

    arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - Print number of arguments
+* @argc: Number if lines arguments
+* @argv: Argument Vector
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc-1);
+return (0);
+}
