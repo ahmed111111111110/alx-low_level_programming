50a1a02c41e6432eca94e1bbecfbecb16cc1645d
ahmed111111111110 <ahmedhuseeny1@gmail.com>
2023-04-11 00:50:37 +0200

    arg

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,13 +6,21 @@
 * main - Print multiplies of two numbers.
 * @argc: Number if lines arguments
 * @argv: Argument Vector
-* Return: 0
+* Return: 0 if sucsses else return 1
 */
+
 int main(int argc, char *argv[])
 {
-int i = atoi(argv[1]) * atoi(argv[2]);
-printf("%d\n", i);
+int x = 3;
+if (argc < x || argc > x)
+{
+printf("Error\n");
+return (1);
+}
+int i = atoi(argv[1]);
+int k = atoi(argv[2]);
+int result = i * k;
+printf("%d\n", result);
 
 return (0);
 }
-
