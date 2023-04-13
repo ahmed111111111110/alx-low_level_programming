5c5d48e32fd05c4b80d7145be16ac02cd33ee134
ahmed111111111110 <ahmedhuseeny1@gmail.com>
2023-04-14 01:11:42 +0200

    My commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,7 +12,6 @@
 int is_digit(char *s)
 {
 int i = 0;
-
 while (s[i])
 {
 if (s[i] < '0' || s[i] > '9')
@@ -21,7 +20,6 @@ i++;
 }
 return (1);
 }
-
 /**
 * _strlen - returns the length of a string
 * @s: string to evaluate
@@ -31,14 +29,12 @@ return (1);
 int _strlen(char *s)
 {
 int i = 0;
-
 while (s[i] != '\0')
 {
 i++;
 }
 return (i);
 }
-
 /**
 * errors - handles errors for main
 */
@@ -47,7 +43,6 @@ void errors(void)
 printf("Error\n");
 exit(98);
 }
-
 /**
 * main - multiplies two positive numbers
 * @argc: number of arguments
@@ -59,7 +54,6 @@ int main(int argc, char *argv[])
 {
 char *s1, *s2;
 int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
-
 s1 = argv[1], s2 = argv[2];
 if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 errors();
