d66711e66a308f99ccc90115380c3bf507ab7c1b
ahmed111111111110 <ahmedhuseeny1@gmail.com>
2023-04-19 23:51:12 +0200

    function_pointers

--- main.h
+++ main.h
@@ -1,5 +1,12 @@
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
