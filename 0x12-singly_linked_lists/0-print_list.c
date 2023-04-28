f274d3898bccad7c7ba6c85cdeaae33801ae36e5
ahmed111111111110 <ahmedhuseeny1@gmail.com>
2023-04-28 10:00:23 +0300

    singly_linked_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,11 +13,16 @@ size_t print_list(const list_t *h)
 size_t count = 0;
 while (h != NULL)
 {
+if (count == 0)
+{
+printf("[0] (nil)\n");
+}
+else
+{
 printf("[%u]%s\n", h->len, h->str);
 count++;
 h = h->next;
-if (count == 0)
-printf("[0] (nil)\n");
+}
 }
 
 return (count);
