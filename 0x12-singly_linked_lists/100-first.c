cbb5c44f9d0ecabef0e474d6eaaf8afe8840050f
ahmed111111111110 <ahmedhuseeny1@gmail.com>
2023-04-28 11:48:12 +0300

    singly_linked_list

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,6 +3,7 @@
 void first(void) __attribute__ ((constructor));
 /**
 * first - prints a sentence before the main
+* function is executed
 */
 void first(void) {
 printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
