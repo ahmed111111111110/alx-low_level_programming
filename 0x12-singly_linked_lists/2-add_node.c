07be21ae9089adb2d73f65187522b6a477a23355
ahmed111111111110 <ahmedhuseeny1@gmail.com>
2023-04-28 10:55:20 +0300

    singly_linked_list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,14 +9,16 @@
 
 */
 
-list_t *add_node(list_t **head, const char *str) {
-list_t *new_node = malloc(sizeof(list_t));
-if (new_node == NULL) {
-return NULL;
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *new_n = malloc(sizeof(list_t));
+if (new_n == NULL)
+{
+return (NULL);
 }
-new_node->str = strdup(str);
-new_node->next = *head;
-*head = new_node;
+new_n->str = strdup(str);
+new_n->next = *head;
+*head = new_n;
 
-return (new_node);
+return (new_n);
 }
