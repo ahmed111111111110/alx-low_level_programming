aa83edf397feaae5d8d591296e1391bf841ca133
Ahmed-Hussieny <ahmedhuseeny1@gmail.com>
2023-05-02 21:50:14 +0300

    more singly linked lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,12 +7,8 @@
 */
 void free_listint2(listint_t **head)
 {
-listint_t *current
-if(head == NULL)
-{
-return (NULL);
-}
-while (current)
+listint_t *current = *head;
+while (current != NULL)
 {
 listint_t *next = current->next;
 free(current);
