0d14b4c90b4b4220ba4a41dbab137b566cd09b67
Ahmed-Hussieny <ahmedhuseeny1@gmail.com>
2023-05-02 22:23:45 +0300

    more singly linked lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,12 +8,11 @@
 * * Return: pointer to the node we're looking for, or NULL if it fails
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index) {
+unsigned int count = 0;
+listint_t *current = head;
 if (head == NULL) {
 return (NULL);
 }
-
-listint_t *current = head;
-unsigned int count = 0;
 while (current != NULL && count < index) {
 current = current->next;
 count++;
