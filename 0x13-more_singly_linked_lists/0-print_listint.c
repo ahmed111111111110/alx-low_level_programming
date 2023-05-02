5eac638e38aaf50da9889fac94873a571aee63ff
Ahmed-Hussieny <ahmedhuseeny1@gmail.com>
2023-05-02 21:06:21 +0300

    more singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,17 +2,17 @@
 #include<unistd.h>
 #include "lists.h"
 /**
- * print_listint - print every element in linked list
- * @h: pointer to linked list
- *
- * Return: The number of nodes
+* print_listint - print every element in linked list
+* @h: pointer to linked list
+*
+* Return: The number of nodes
 */
 
 size_t print_listint(const listint_t *h)
 {
 size_t x = 0;
 
-while (h != NULL)
+while (h)
 {
 printf("%d\n", h->n);
 h = h->next;
