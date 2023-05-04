ffdb95813e5a5951ac73fdf80f644426353861bf
Ahmed-Hussieny <ahmedhuseeny1@gmail.com>
2023-05-04 14:03:33 +0300

    bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,9 +7,8 @@
 */
 
 void print_binary(unsigned long int n) {
-unsigned i ;
-    for ( i = 1 << 31; i > 0 ; i = i / 2) {
-        (n & i) ? _putchar('1') : _putchar('0');
-    }
+    unsigned i;
+    for (i = 1 << 31; i > 0; i = i / 2)
+        (n & i) ? printf("1") : printf("0");
 
 }
