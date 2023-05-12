fef843e4e643587fdf67d04ceb2a04acd9972b1e
ahmed111111111110 <ahmedhuseeny1@gmail.com>
2023-05-12 15:48:40 +0300

    file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,9 @@
 #include <stddef.h>
-#include "main.h"
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 
 
@@ -18,14 +16,16 @@
 * or zero or null if fails
 */
 
-size_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
+ssize_t too;
+ssize_t wr;
 char *bud = malloc(sizeof(char) * letters);
 ssize_t fgg = open(filename, O_RDONLY);
 if (fgg == -1)
 return (0);
-ssize_t too = read(fgg, bud, letters);
-ssize_t wr = write(STDOUT_FILENO, bud, too);
+too = read(fgg, bud, letters);
+wr = write(STDOUT_FILENO, bud, too);
 free(bud);
 close(fgg);
 return (wr);
